036146940b542fafdcd06e00e64ad01009d84749
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-04-03 12:09:35 -0400

    adding cover

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 function Home(props) {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <h2>hi</h2>
-                </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="introduction">
+            <div className="caption">
+              <h1 className="text-center text-white"><strong>We help people with</strong></h1>
+              <h2 className="text-center text-white">Adulting • Finances • Gardening • Homesteading</h2>
+              <button type="button" className="btn btn-success btn-lg" data-toggle="modal" data-target="#signUpModal">Sign up now!</button>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Home;
